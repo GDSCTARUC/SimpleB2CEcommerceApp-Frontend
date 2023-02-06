f6b608bf0fdc2be4e8046ace98554da51ed8bb1b
Kayin <34983642+kayinleong@users.noreply.github.com>
2023-02-06 14:13:10 +0800

    Update OIDC Configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
 	production: true,
+	oidcProfile: 'frontend',
 	urls: {
 		authUrl: 'https://gdsctarumt-openid.azurewebsites.net',
 		apiUrl: 'https://gdsctarumt-apigateway.azurewebsites.net',
